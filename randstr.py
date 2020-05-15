f4661267c53a9ca75c73558733f67184abffd5ec
unknown <tylerdesu@icloud.com>
2020-05-16 07:09:20 +0900

    updated rand_str.py

--- randstr.py
+++ randstr.py
@@ -1,12 +1,27 @@
-#Generate an ostensibly random string.
+# Generate an ostensibly random string
+# with equal probability of each character
+# being either a letter, number, or special
+# character
+
 import random
 import sys
 
 def main():
+	nums = [chr(i) for i in range(48,58)]
+	upper_case_letters = [chr(i) for i in range(65,91)]
+	lower_case_letters = [chr(i) for i in range(97,123)]
+	symbols = [chr(i) for i in range(33,48)]
+	symbols += [chr(i) for i in range(59,64)]
 	pw = ""
-	for i in range(1,22):
-		pw += chr(random.randrange(48,126))
+	pw_char_lists = [nums,upper_case_letters,lower_case_letters,symbols]
+	
+	for i in range(1,25):
+		selected_pw_char_list = pw_char_lists[random.randrange(0,4)]
+		pw_char = random.choice(selected_pw_char_list)
+		pw += pw_char
+
 	print(pw)
 
 if __name__=="__main__":
-	main()	
+	for i in range (1,20):
+		main()
